b87a5b0e064ecab8b9773a7bbf838dc8e0672c57
Jared Richardson <jr952390@cameron.edu>
2025-04-15 17:25:43 -0500

    style should now be loading properly and paragraphs for the tabs has been created for php input later.

--- code/frontend/student_home.php
+++ code/frontend/student_home.php
@@ -1,4 +1,4 @@
-<?php
+<p?php
 session_start();
 
 
@@ -28,142 +28,72 @@ $conn->close();
 ?>
 
 <!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Student Home - UniEnroll</title>
-        <link rel="stylesheet" href="../../assets/css/home.css">
-        <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-    </head>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student Home - UniEnroll</title>
+    <link rel="stylesheet" href="../../assets/css/style.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
 
-    <body class="admin">
-        <ul>
-            <img src="../../assets/css/cameron.png" alt="Student Picture">
-            <h2>STUDENT INFORMATION</h2>
-            <p><?php echo $student_name; ?></p>
-            <p>Class</p>
-            <p><?php echo $classification; ?></p>
-            <p>Major</p>
-            <p><?php echo $major; ?></p>
-            <p>Minor</p>
-            <p><?php echo $minor; ?></p></ul>
+<body>
+    <ul class="sidebar">
+            <img src="../../assets/images/cameron.png" class="logo">
+            <li><a>Student Information</a></li>
+            <li><a href="#"id="contact-tab">Contact</a></li>
+            <li><a href="../login.html"><i class="bx bx-log-out"></i>Logout</a></li>
+    </ul>
 
-            <form action="../middleend/process_logout.php" method="post">
-                <button type="submit" name="logout" style="background: none; border: none; color: inherit; text-decoration: none;">Sign Out</button>
-            </form>
-        
-        <div class="action-box">
-            <div class="tabs"> 
-                <ol>
-                    <li class="active">
-                        <span class="icon"><i class='bx bxs-user'></i></span>
-                        <span class="text">Current Schedule</span>
-                    </li>
-
-                    <li>
-                        <span class="icon"><i class='bx bxs-cog'></i></span>
-                        <span class="text">Available Courses</span>
-                    </li>
-
-                    <li>
-                        <span class="icon"><i class='bx bxs-buildings'></i></span>
-                        <span class="text">Organizations</span>
-                    </li>
-                    <li>
-                        <span class="icon"><i class='bx bxs-buildings'></i></span>
-                        <span class="text">Internships</span>
-                    </li>
-
-                    <li>
-                        <span class="icon"><i class='bx bxs-door-open'></i></span>
-                        <span class="text">Contact</span>
-                    </li>
-                </ol>
-            </div>
-
-            <div class="content">
-                <div class="tab_wrap" style="display: block;">
-                    <div class="title">Your Current Schedule</div>
-                    <div class="tab-content">
-                        <table>
-                            <tr>
-                                <th>Course ID</th>
-                                <th>Course Name</th>
-                                <th>Professor</th>
-                                <th>Building</th>
-                                <th>Room</th>
-                                <th>Time</th>
-                                <th>Days</th>
-                            </tr>
-                            <tr>
-                                <td>4050</td>
-                                <td>Capstone</td>
-                                <td>Johnny Appleseed</td>
-                                <td>Howell Hall</td>
-                                <td>203</td>
-                                <td>2 pm</td>
-                                <td>M W</td>
-                            </tr>
-                            <tr>
-                                <td>More</td>
-                                <td>Test</td>
-                                <td>Data</td>
-                                <td></td>
-                            </tr>
-                        </table>
-                    </div>
-                </div>
-
-                <div class="tab_wrap" style="display: none;">
-                    <div class="title">Potential courses</div>
-                    <div class="tab-content">
-                        <p>In this tab you will be able to add and delete classes as long as you have a pin from your advisor.</p>
-                        <table>
-                            <tr>
-                                <th>Course ID</th>
-                                <th>Course Name</th>
-                                <th>Professor</th>
-                                <th>Building</th>
-                                <th>Room</th>
-                                <th>Time</th>
-                                <th>Days</th>
-                            </tr>
-                            <tr>
-                                <td>4050</td>
-                                <td>Capstone</td>
-                                <td>Johnny Appleseed</td>
-                                <td>Howell Hall</td>
-                                <td>203</td>
-                                <td>2 pm</td>
-                                <td>M W</td>
-                            </tr>
-                            <tr>
-                                <td>More</td>
-                                <td>Test</td>
-                                <td>Data</td>
-                                <td></td>
-                            </tr>
-                        </table>
-                    </div>
-                </div>
-
-                <div class="tab_wrap" style="display: none;">
-                    <div class="title">Manage Buildings</div>
-                    <div class="tab-content">
-                        <p>In this tab you will be able to add and delete school buildings in the course descriptions.</p>
-                    </div>
-                </div>
-
-                <div class="tab_wrap" style="display: none;">
-                    <div class="title">Manage Rooms</div>
-                    <div class="tab-content">
-                        <p>In this tab you will be able to add and delete classrooms in the course descriptions.</p>
-                    </div>
-                </div>
-            </div>
+    <div class="tabs-container">
+        <div class="tabs">
+            <div class="tab" onclick="showContent('Class and Schedule')">Class and Schedule</div>
+            <div class="tab" onclick="showContent('Class Enrollment')">Class Enrollment</div>
+            <div class="tab" onclick="showContent('Organizations')">Organizations</div>
+            <div class="tab" onclick="showContent('Internship')">Internship</div>
         </div>
-        
-        <script src="scripts.js"></script>
-    </body>
+
+    <div class="sign-out">
+        <form action="../middleend/process_logout.php" method="post">
+            <button type="submit" name="logout" style="background: none; border: none; color: inherit; text-decoration: none;">Sign Out</button>
+        </form>
+    </div>
+
+    </div>
+
+    <div id="Class and Schedule" class="content active">
+        <h2>Class and Schedule</h2>
+        <p>Here you can see the classes you are currently enrolled in and their date/time.</p>
+        <p> EXAMPLE </p> 
+    </div>
+
+    <div id="Class Enrollment" class="content">
+        <h2>Class Enrollment</h2>
+        <p>In this tab you will be able to enroll into other classes as long as you have been given your access pin by your advisor.</p>
+        <p></p>
+    </div>
+
+    <div id="Organizations" class="content">
+        <h2>Organizations</h2>
+        <p>Here are a list of the organizations that you are a part of, as well as potential organizations you could join.</p>
+        <p></p>
+    </div>
+
+    <div id="Internship" class="content">
+        <h2>Internship</h2>
+        <p>This tab is for any Internships you are a part of or if you wish to apply for an internship.</p>
+        <p></p>
+    </div>
+
+    <script>
+        function showContent(theme) {
+            const contents = document.querySelectorAll('.content');
+            contents.forEach(content => {
+                content.classList.remove('active');
+            });
+            document.getElementById(theme).classList.add('active');
+        }
+    </script>
+
+</body>
 </html>
