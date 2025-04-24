a5f098abfccbf6f2340cc927c3d4af3bde4f85ba
Lexi Baggett <lexibaggett@Lexis-MacBook-Pro.local>
2025-04-23 21:02:19 -0500

    Made changes to db, test data, and add_course.php.

--- database/test_data.sql
+++ database/test_data.sql
@@ -43,8 +43,8 @@ INSERT INTO `faculty` (`id`, `firstname`, `lastname`, `office`, `email`, `phonen
 (105, 'Jake', 'Brown', 'Office 106', 'jbrown@example.com', '1473859764', 'faculty'),
 (106, 'Logan', 'Williams', 'Office 107', 'lwilliams@example.com', '1394003428', 'faculty'),
 (107, 'James', 'Miller', 'Office 109', 'jmiller@example.com', '0345670385', 'faculty'),
-(108, 'Laura', 'Jones', 'Office 105', 'ljones@example.com', '9845342343', 'faculty');
-
+(108, 'Laura', 'Jones', 'Office 105', 'ljones@example.com', '9845342343', 'faculty')
+(109, 'Jacob', 'Flores', 'Office 206', 'jflores@example.com', '6827193842', 'advisor');
 -- Insert data for table `student`
 INSERT INTO `student` (`studentid`, `firstname`, `lastname`, `email`, `classification`, `degree`, `major`, `minor`, `orgid`, `internid`) VALUES
 (1000, 'Tom', 'Brown', 'tbrown@example.com', 'Sophomore', 'B.Sc. Computer Science', 'Computer Science', 'Mathematics', NULL, NULL),
@@ -52,7 +52,9 @@ INSERT INTO `student` (`studentid`, `firstname`, `lastname`, `email`, `classific
 (1002, 'James', 'White', 'jwhite@example.com', 'Freshman', 'B.Sc. Computer Science', 'Computer Science', 'Physics', 3, 1),
 (1003, 'Dylan', 'Smith', 'dsmith@example.com', 'Senior', 'B.Sc. Sociology', 'Sociology', NULL, NULL, 3),
 (1004, 'Jake', 'Simmons', 'jsimmons@example.com', 'Freshman', 'B.Sc. Psychology', 'Psychology', NULL, NULL, NULL),
-(1005, 'Robert', 'Addams', 'raddams@example.com', 'Junior', 'B.Sc. Biology', 'Biology', NULL, NULL, NULL);
+(1005, 'Robert', 'Addams', 'raddams@example.com', 'Junior', 'B.Sc. Biology', 'Biology', NULL, NULL, NULL),
+(1006, 'Mason', 'Reed', 'mreed@example.com', 'Senior', 'M.A. Master of Arts','Psychology', NULL, NULL, NULL), 
+(1007, 'Emily', 'Watson', 'ewatson@example.com', 'Sophmore', 'M.A. Master of Arts', 'Communication', NULL, NULL, NULL);
 
 -- Insert data for table `course`
 INSERT INTO `course` (`courseid`, `coursedesc`, `building`, `room`, `time`, `days`, `facultyid`) VALUES
@@ -116,8 +118,15 @@ INSERT INTO `room` (`roomid`, `roomdesc`, `orderby`, `isactive`, `buildingid`) V
 
 -- Insert data for table `time`
 INSERT INTO `time` (`timeid`, `timedesc`, `orderby`, `isactive`) VALUES
-(1, 'Morning', 1, 1),
-(2, 'Afternoon', 2, 1),
-(3, 'Evening', 3, 0);
+(1, '8:00 am – 9:15 am', 1, 1),
+(2, '9:30 am – 10:45 am', 2, 1),
+(3, '11:00 am – 12:15 pm', 3, 0),
+(4, '12:30 pm – 1:45 pm', 4, 0), 
+(5, '2:00 pm – 3:15 pm', 5, 0), 
+(6, '3:30 pm – 4:45 pm', 6, 0);
+
+
+
+
 
 
